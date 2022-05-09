68abd11b6178771a58fecb65ca3a280fadd5f707
IvanTron <99278095+IvanRoger20@users.noreply.github.com>
2022-05-09 17:40:54 -0600

    Add files via upload

---
+++ src/eva3_2_excepciones/EVA3_2_EXCEPCIONES.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_2_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_2_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu edad(Numero entero): ");
+        edad = sc.nextInt();
+        System.out.println("Tu edad: " + edad);
+    }
+    
+}
